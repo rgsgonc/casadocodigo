838c13977e07f97fed6691df41fe3903324c50cf
Rafael <rgs.gonc@gmail.com>
2017-11-07 00:46:12 -0200

    cadastro do preco do produto

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -3,15 +3,21 @@ package br.com.casadocodigo.loja.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
 public class ProdutosController {
 	@RequestMapping("/produtos/form")
-    public String form(){
-        return "produtos/form";
+    public ModelAndView form(){
+
+        ModelAndView modelAndView = new ModelAndView("produtos/form");
+        modelAndView.addObject("tipos", TipoPreco.values());
+
+        return modelAndView;
     }
 	
 	@Autowired
--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,9 +16,18 @@ public class Produto {
 	private String titulo;
     private String descricao;
     private int paginas;
+    @ElementCollection
+    private List<Preco> precos;
+    
+    
 
-
-    public String getTitulo() {
+    public List<Preco> getPrecos() {
+		return precos;
+	}
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
+	public String getTitulo() {
         return titulo;
     }
     public void setTitulo(String titulo) {
